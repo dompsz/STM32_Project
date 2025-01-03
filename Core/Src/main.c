190fe083fb8522b7accc13a13871ce11546bca6c
dompsz <dompszcz@gmail.com>
2025-01-03 06:14:14 +0100

    functional circular buffer with console debug

---
+++ Core/Src/main.c
@@ -0,0 +1,714 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef enum
+{
+	FrameStart,
+	Source,
+	Destination,
+	CommandLength,// if 00 go to data
+	Command,
+	Data,
+	Argument,
+	FrameEnd
+} FrameDetection;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define TX_BUF_LEN 600
+#define RX_BUF_LEN 600
+#define CRC8_POLYNOMIAL 0x07
+#define CRC8_INITIAL_VALUE 0x00
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+static FrameDetection detection = FrameStart;
+uint8_t frame_index = 0;
+char src[3];
+char dst[3];
+char cmd_len[3];
+uint8_t cmd_length = 14;
+uint8_t current_cmd_length = 0;
+char data[33];
+char cmd[31]; //32 - '[]' + '\0'
+char cmd_arg[30]; // argument 9 = 14 - (najkrótsza komenda + '[' + ']')
+char checksum[3];
+char frame[43];
+
+//volatile uint32_t current_delay = 1000;//ms
+volatile uint32_t current_interval = 0;
+volatile bool display_logs = true; //TODO implement functionality
+
+volatile uint8_t BUF_RX[RX_BUF_LEN];
+volatile uint8_t BUF_TX[TX_BUF_LEN];
+
+volatile uint16_t empty_RX = 0;
+volatile uint16_t busy_RX = 0;
+volatile uint16_t empty_TX = 0;
+volatile uint16_t busy_TX = 0;
+
+//command errors
+char* new_line = "\r\n\r";
+char* e_cmd_too_long = "Command is too long";
+char* e_cmd = "Unknown command";
+char* e_arg = "Argument Error";
+char* e_arg_too_long = "Argument Too Long";
+char* e_index = "Invalid Index";
+char* e_crc = "Validation Failed";
+char* e_frame = "Frame error";
+char* e_data_too_long = "Data too long";
+char* e_len = "Command length error";
+char* e_address = "Address Error";
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_USART2_UART_Init(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+int8_t USART_get_char() {//pobieranie z bufora odbiorczego
+    // Sprawdź, czy bufor odbiorczy jest pełny
+    if (busy_RX == empty_RX) {
+        return -1;
+    }
+    // Pobierz znak z bufora
+    int8_t sign = BUF_RX[busy_RX];
+    busy_RX++;
+    if (busy_RX >= RX_BUF_LEN) {
+        busy_RX = 0; // zapętlenie bufora
+    }
+    return sign; // Zwróć odebrany znak
+}
+
+void USART_send(char* message){ //wysyłanie do bufora TX
+    uint16_t i,idx = empty_TX;
+
+    for(i=0; message[i] != '\0'; i++){ // przenosimy tekst z wywołania funkcji USART_send do tablicy BUF_TX[]
+
+    	BUF_TX[idx] = message[i];
+        idx++;
+        if(idx >= TX_BUF_LEN) idx = 0;//zapętlenie idx
+    } // cały tekst message[] znajduje się już teraz w BUF_TX[]
+    __disable_irq(); //wyłączamy przerwania, bo poniżej kilka linii do zrobienia
+    if ((busy_TX == empty_TX)&&(__HAL_UART_GET_FLAG(&huart2, UART_FLAG_TXE)==SET))
+      	//sprawdza czy bufor jest pusty
+       	//i czy flaga jest ustawiona(czy jest gotowy do ponownej komunikacji)
+    {
+        empty_TX = idx;
+        uint8_t tmp = BUF_TX[busy_TX];//znak do wysłania
+        busy_TX++;
+        if(busy_TX >= TX_BUF_LEN) busy_TX = 0;//zapętla bufor
+        HAL_UART_Transmit_IT(&huart2, &tmp, 1);
+    }
+    else
+    {
+        empty_TX = idx;
+    }
+    __enable_irq(); //ponownie aktywujemy przerwania
+}
+
+//Resetuje wszystkie tablice ramki i nasłuchiwanie
+void Frame_reset() {
+	detection = FrameStart;
+	frame_index = 0;
+	cmd_length = 14;
+	current_cmd_length = 0;
+	memset(src, '\0', sizeof(src));//zapełnia tablice pustymi znakami
+	memset(dst, '\0', sizeof(dst));
+	memset(cmd_len, '\0', sizeof(cmd_len));
+	memset(cmd, '\0', sizeof(cmd));
+	memset(cmd_arg, '\0', sizeof(cmd_arg));
+	memset(data, '\0', sizeof(data));
+	memset(checksum, '\0', sizeof(checksum));
+	memset(frame, '\0', sizeof(frame));
+}
+
+////koduje znaki
+//void Cipher(char* tbl, char a, char b) {
+//	uint8_t limit = strlen(tbl);
+//	if (frame_index > limit - 1) {
+//		USART_send(e_cmd_too_long);
+//		Frame_reset();
+//	} else {
+//		tbl[frame_index] = a;
+//		frame_index++;
+//		tbl[frame_index] = b;
+//		frame_index++;
+//	}
+//}
+
+////rozpoczyna kodowanie jeżeli natrafimy na konkretne znaki
+//void Cipher_chain(char* tbl, char sign) {
+//	if		(sign == '<')	Cipher(tbl,'/','1');
+//	else if (sign == '>')	Cipher(tbl,'/','2');
+//	else if (sign == '/')	Cipher(tbl,'/','/');
+//}
+
+//sprawdza poprawność rozmiaru tabli, true - error
+bool Size_check(char* tbl, char* error) {
+	if (frame_index > strlen(tbl)) {
+		USART_send(new_line);
+		USART_send(error);
+		USART_send(new_line);
+		Frame_reset();
+		return true;
+	}
+	return false;
+}
+
+// sprawdza czy kluczowe elementy ramki nie są puste (adresy/crc), true - error
+bool Empty_check(char* tbl){
+	if (strlen(tbl) == 2) {
+		return false;
+	}
+	USART_send(new_line);
+	USART_send(e_address);
+	USART_send(new_line);
+	Frame_reset();
+	return true;
+}
+
+// Funkcja do odwracania bitów w bajcie do crc
+uint8_t Reverse_bits(uint8_t byte) {
+    uint8_t reversed = 0;
+    for (uint8_t i = 0; i < 8; i++) {
+        if (byte & (1 << i)) {
+            reversed |= (1 << (7 - i));
+        }
+    }
+    return reversed;
+}
+
+// Funkcja obliczająca CRC8 dla ciągu znaków
+uint8_t Calculate_CRC8(char* data) {
+    uint8_t crc = CRC8_INITIAL_VALUE;
+    uint8_t length = strlen(data);  // Długość ciągu znaków
+
+    for (uint8_t i = 0; i < length; i++) {
+        uint8_t byte = data[i];
+        byte = Reverse_bits(byte);  // Odwrócenie bitów w bajcie
+        crc ^= byte;  // XOR z bieżącym bajtem
+
+        for (uint8_t j = 0; j < 8; j++) {
+            if (crc & 0x80) {//sprawdza MSB
+                crc = (crc << 1) ^ CRC8_POLYNOMIAL;  // Przesunięcie i XOR z wielomianem
+            } else {
+                crc <<= 1;  // Tylko przesunięcie
+            }
+        }
+    }
+
+    return Reverse_bits(crc);  // Odwrócenie bitów CRC na koniec
+}
+
+//dokleja tablice x do gotowej ramki
+void Frame_merge(char* tbl) {
+	for (uint8_t i = 0;i < strlen(tbl);i++) {
+		frame[frame_index] = tbl[i];
+		frame_index++;
+	}
+}
+
+//łączy wszystkie tablice ramki danych i dodaje sume kontrolną
+void Merge_data_frame() {
+	frame_index = 0;
+	frame[frame_index++] = '<';
+	Frame_merge(src);
+	Frame_merge(dst);
+	Frame_merge(cmd_len);
+	Frame_merge(data);
+	uint8_t crc = Calculate_CRC8(frame);
+	char crc_str[3]; // 2 cyfry plus '\0'
+	snprintf(crc_str, sizeof(crc_str), "%02X", crc); // konwersja hex do char
+	Frame_merge(crc_str);
+	frame[frame_index++] = '>';
+	frame[frame_index] = '\0';
+}
+
+//łączy wszystkie tablice ramki komendy i dodaje sume kontrolną
+void Merge_Command_frame() {
+	frame_index = 0;
+	frame[frame_index++] = '<';
+	Frame_merge(src);
+	Frame_merge(dst);
+	Frame_merge(cmd_len);
+	Frame_merge(cmd);
+	frame[frame_index++] = '[';
+	Frame_merge(cmd_arg);
+	frame[frame_index++] = ']';
+	uint8_t crc = Calculate_CRC8(frame);
+	char crc_str[3]; // 2 cyfry plus '\0'
+	snprintf(crc_str, sizeof(crc_str), "%02X", crc); // konwersja hex do char
+	Frame_merge(crc_str);
+	frame[frame_index++] = '>';
+	frame[frame_index] = '\0';
+}
+
+void Frame_compare() {
+	USART_send(new_line);
+	if (current_cmd_length == 0) {//data
+		Merge_data_frame();
+		USART_send(frame);
+		Frame_reset();
+		USART_send(new_line);
+		return;
+	} else {
+		Merge_Command_frame();
+		USART_send(frame);
+		USART_send(new_line);
+	}
+
+	if (strcmp(cmd, "cur") == 0){// porównanie stringa z komendą
+		if (strcmp(cmd_arg, "t") == 0){
+			USART_send("temperature: 25C");//argumenty
+		} else if (strcmp(cmd_arg, "m") == 0) {
+			USART_send("moisture: 20%");//argumenty
+		} else if (strcmp(cmd_arg, "\0") == 0) {
+			USART_send("temp: 25C, moist: 20%");//argumenty
+		} else USART_send(e_arg);
+
+	}else if (strcmp(cmd, "int") == 0) {// ustawia/ wyświetla interwał
+		uint32_t tmp_int_from_frame = atoi(cmd_arg);
+		if (strcmp(cmd_arg, "\0") == 0){//show
+			char tmp_curent_int[11];//max 4 miliony + \0
+			sprintf(tmp_curent_int, "%u", current_interval);
+			USART_send("Current Interval: ");
+			USART_send(tmp_curent_int);
+		} else {
+			if (tmp_int_from_frame > 0) {//set
+				current_interval = tmp_int_from_frame;
+				char tmp_curent_int[11];//max 4 miliony + \0
+				sprintf(tmp_curent_int, "%u", current_interval);
+				USART_send("Interval set to: ");
+				USART_send(tmp_curent_int);
+			} else USART_send(e_arg);
+		}
+
+	}else if (strcmp(cmd, "log") == 0) {
+		if (strcmp(cmd_arg, "y") == 0 || strcmp(cmd_arg, "t") == 0 ){
+			display_logs = true;
+			USART_send("Interval Logs ON");
+		} else if (strcmp(cmd_arg, "n") == 0 || strcmp(cmd_arg, "f") == 0 ) {
+			display_logs = false;
+			USART_send("Interval Logs OFF");
+		} else USART_send(e_arg);
+
+	}else if (strcmp(cmd, "data") == 0) {//TODO implement functionality
+		if (strcmp(cmd_arg, "\0") == 0){
+			for(uint8_t i = 0 ; i < 5 ; i++) {
+				USART_send("12:10 17.11.2024: 20C, 20%");
+				USART_send(new_line);
+			}
+		} else{
+	//		uint16_t tmp_arg = atoi(cmd_arg);
+			if (1) {//TODO check if table has this index
+				USART_send("data with given index: ");
+				USART_send("12:10 17.11.2024: 20C, 20%");
+			} else USART_send(e_index);
+		}
+
+	} else if (strcmp(cmd, "purge") == 0) { //TODO implement functionality
+		if (strcmp(cmd_arg, "\0") == 0){
+			USART_send("Cleared all data");
+		}else USART_send(e_arg);
+
+	} else USART_send(e_cmd);
+
+	Frame_reset();
+	USART_send(new_line);
+}
+
+void FrameRd()
+{
+	int8_t x = USART_get_char();
+	if (x <= 0) {return;}// Upewnia się, że znak został poprawnie odebrany
+	char sign = x;
+
+	if (sign == '<'){
+		Frame_reset();
+		detection = Source;
+		return;
+	}
+
+	if (sign == '>'){
+		bool correct_frame = true;
+		if (Size_check(src, e_address))			correct_frame = false;
+		if (Size_check(dst, e_address))			correct_frame = false;
+		if (Size_check(cmd_len, e_len))			correct_frame = false;
+		if (Size_check(cmd, e_cmd_too_long))	correct_frame = false;
+		if (Size_check(cmd_arg, e_arg_too_long))correct_frame = false;
+		if (Size_check(data, e_data_too_long))	correct_frame = false;
+		if (Empty_check(src)) return;
+		if (Empty_check(dst)) return;
+
+		if (correct_frame) Frame_compare();
+		else Frame_reset();
+		return;
+	}
+
+	switch (detection){
+		case FrameStart:
+			break;
+		case FrameEnd:
+			break;
+		case Source:
+			src[frame_index] = sign;
+			frame_index++;
+			if (frame_index == 2){
+				src[frame_index] = '\0';
+				frame_index = 0;
+				detection = Destination;
+			}
+			break;
+		case Destination:
+			dst[frame_index] = sign;
+			frame_index++;
+			if (frame_index == 2){
+				src[frame_index] = '\0';
+				frame_index = 0;
+				detection = CommandLength;
+			}
+			break;
+		case CommandLength:
+			cmd_len[frame_index] = sign;
+			frame_index++;
+			if (frame_index == 2){
+				cmd_len[frame_index] = '\0';
+				frame_index = 0;
+				detection = Command;
+				cmd_length = atoi(cmd_len);
+				if (strcmp(cmd_len, "00") == 0) {
+					detection = Data;
+					cmd_length = 0;
+				}
+			}
+			if (!isdigit(sign)) {
+				USART_send(new_line);
+				USART_send(e_len);
+				Frame_reset();
+			}
+			break;
+		case Command:
+			if(Size_check(cmd, e_cmd_too_long)) return;
+			if (sign == '['){	//zaczyna argument
+				cmd[frame_index] = '\0';
+				frame_index = 0;
+				cmd_length -= current_cmd_length;
+				detection = Argument;
+			} else{
+				cmd[frame_index] = sign; //sprawdza reszte liter komendy
+				frame_index++;
+				current_cmd_length++;
+			}
+			if (frame_index > cmd_length) {
+				USART_send(new_line);
+				USART_send(e_cmd_too_long);
+				Frame_reset();
+			}
+			break;
+		case Argument:
+			if (Size_check(cmd_arg, e_arg_too_long)) return;
+			if (sign == ']'){  // konczy argument
+				cmd_arg[frame_index] = '\0';
+				frame_index = 0;
+				detection = FrameEnd;
+			}
+			else{
+				cmd_arg[frame_index] = sign;
+				frame_index++;
+			}
+			if (frame_index > cmd_length) {
+				USART_send(new_line);
+				USART_send(e_arg_too_long);
+				Frame_reset();
+			}
+			break;
+		case Data:
+			if (Size_check(data, e_data_too_long)) return;
+			data[frame_index] = sign;
+			frame_index++;
+			if (frame_index == 32) {
+				data[frame_index] = '\0';
+				frame_index = 0;
+				detection = FrameEnd;
+			}
+			break;
+	}
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){//wypisywanie w konsoli
+	if(huart == &huart2) {//weryfikacja poprawności wskaźnika
+		if(busy_TX != empty_TX){
+			uint8_t tmp = BUF_TX[busy_TX];
+			busy_TX++;
+			if(busy_TX >= TX_BUF_LEN) busy_TX = 0;//zapętlenie indexu
+			HAL_UART_Transmit_IT(&huart2, &tmp, 1);
+		}
+    }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){//odbieranie z konsoli
+	if(huart == &huart2) {//weryfikacja poprawności wskaźnika
+		if ((empty_RX + 1) % RX_BUF_LEN != busy_RX) {
+			//index update
+			empty_RX++;
+			if(empty_RX >= RX_BUF_LEN)	empty_RX = 0;// zapętlenie bufora
+			HAL_UART_Receive_IT(&huart2, &BUF_RX[empty_RX], 1);
+		}
+		HAL_UART_Transmit_IT(&huart2, &BUF_RX[empty_RX-1], 1);
+		//wyświetla ostatni odebrany znak w konsoli do debugowania
+	}
+}
+
+//void Delay()
+//{
+//	uint32_t start_time = uwTick; // aktualny czas z systick handlera
+//	    while ((uwTick - start_time) < current_delay)
+//	    {
+//	        // Pętla oczekująca
+//	    }
+//}
+
+void welcome() {
+	USART_send(new_line);
+	USART_send("Hi!!!");
+	USART_send(new_line);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  welcome();
+  Frame_reset();
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+//  SysTick_Config(16000000/1000);
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_IT(&huart2, &BUF_RX[0],1);
+  HAL_UART_RxCpltCallback(&huart2);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  FrameRd();
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/* Enable DMA for RX and TX */
+//	HAL_UART_Receive_DMA(&huart2, BUF_RX, RX_BUF_LEN);
+//	HAL_UART_Transmit_DMA(&huart2, BUF_TX, TX_BUF_LEN);
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
